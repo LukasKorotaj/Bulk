0a9113791b8c6f52dfd2afd48f8239e4b5b37d3a
LukasKorotaj <lu0026ko-s@student.lu.se>
2025-01-15 12:58:25 +0100

    Added something

--- src/main/scala/bulk/Main.scala
+++ src/main/scala/bulk/Main.scala
@@ -3,26 +3,26 @@ import java.io.{File, PrintWriter}
 
 object Main:
 
-  def inputString(): String =
+  def inputString(input: String): String =
     try
-      scala.io.StdIn.readLine("> ").toLowerCase()
+      scala.io.StdIn.readLine(input).toLowerCase()
     catch 
       case e: IllegalArgumentException =>
         println("Invalid input")
-        inputString()
+        inputString(input)
 
-  def inputDouble(): Double =
+  def inputDouble(input: String): Double =
     try
-      scala.io.StdIn.readLine("> ").toDouble
+      scala.io.StdIn.readLine(input).toDouble
     catch
       case e: IllegalArgumentException =>
         println("Invalid input")
-        inputDouble()
+        inputDouble(input)
     
   def addIngredientToFile(): Unit =
-    val name = inputString()
-    val proteinPer100 = inputDouble()
-    val caloriesPer100 = inputDouble()
+    val name = inputString("Name of the ingredient: ")
+    val proteinPer100 = inputDouble("Insert protein per 100g: ")
+    val caloriesPer100 = inputDouble("Insert calorie per 100g: ")
     val file = File("ingredients.txt")
     val writer = PrintWriter(file)
     try
@@ -32,8 +32,7 @@ object Main:
     
   @main def run(args: String*): Unit = 
     var continue = true
-    while continue do
-      println("""
+    println("""
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @                         BULK                          @
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -46,7 +45,9 @@ help you get your macros!
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @                                                       @
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
+""")
+    while continue do
+      println("""
 Options: 
 1...Optimise Meal.
 2...Add meal and recipe (optional).
@@ -55,5 +56,25 @@ Options:
 5...Choose random meal.
 6...Make a week plan (random or not).
 7...List recipe for meal (option to save to file)
+8...Quit.
 """)
+
+      val selection = 
+        scala.io.StdIn.readLine("> ")
+      selection match
+      case "1" => println("not implemented")
+      case "2" => println("not implemented")
+      case "3" => addIngredientToFile()
+      case "4" => println("not implemented")
+      case "5" => println("not implemented")
+      case "6" => println("not implemented")
+      case "7" => println("not implemented")
+      case "8" => println("not implemented")
+        println("Quitting...")
+        Thread.sleep(1000)
+        continue = false
+      case _   => 
+        println("Invalid")
+        Thread.sleep(1000)
+         
 
